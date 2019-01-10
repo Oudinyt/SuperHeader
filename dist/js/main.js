cb93c029844869a82163aa30bb77c35e37969ca5
Oody <oudinyt@gmail.com>
2019-01-10 02:10:37 +0100

    Add files via upload

---
+++ dist/js/main.js
@@ -0,0 +1,12 @@
+/*!
+ * project-name v0.0.1
+ * A description for your project.
+ * (c) 2019 YOUR NAME
+ * MIT License
+ * http://link-to-your-git-repo.com
+ */
+
+document.addEventListener('click', (function (event) {
+	if (!event.target.matches('#click-me')) return;
+	alert('You clicked me!');
+}), false);
---
+++ dist/js/main.polyfills.js
@@ -0,0 +1,19 @@
+/*!
+ * project-name v0.0.1
+ * A description for your project.
+ * (c) 2019 YOUR NAME
+ * MIT License
+ * http://link-to-your-git-repo.com
+ */
+
+document.addEventListener('click', (function (event) {
+	if (!event.target.matches('#click-me')) return;
+	alert('You clicked me!');
+}), false);
+/**
+ * Element.matches() polyfill (simple version)
+ * https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
+ */
+if (!Element.prototype.matches) {
+	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
