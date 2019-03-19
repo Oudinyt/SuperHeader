80a90a1120004bc535631b696e268189ed8e5783
Oudinyt <oudinyt@gmail.com>
2019-03-19 13:57:10 +0100

    header left

--- src/js/main/some-code.js
+++ src/js/main/some-code.js
@@ -1,4 +1,10 @@
-document.addEventListener('click', function (event) {
-	if (!event.target.matches('#click-me')) return;
-	alert('You clicked me!');
-}, false);
+// document.addEventListener('click', function (event) {
+// 	if (!event.target.matches('#click-me')) return;
+// 	alert('You clicked me!');
+// }, false);
+//
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+});
