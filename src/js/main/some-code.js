e1862e5eda31cde65dfa78f2255d664738b6d74a
Oudinyt <oudinyt@gmail.com>
2019-03-20 15:34:41 +0100

    header left

--- src/js/main/some-code.js
+++ src/js/main/some-code.js
@@ -1,10 +1,38 @@
-// document.addEventListener('click', function (event) {
-// 	if (!event.target.matches('#click-me')) return;
-// 	alert('You clicked me!');
-// }, false);
-//
+// Breakpoint
+var breakpoint = {};
+
+breakpoint.refreshValue = function () {
+    this.value = window.getComputedStyle(document.querySelector('body'), ':before').getPropertyValue('content').replace(/\"/g, '');
+};
 
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("DOM fully loaded and parsed");
+// Resize
+window.addEventListener("resize", function(event) {
+
+    breakpoint.refreshValue();
+
+    //console.log('resize Breakpoint ' + breakpoint.value);
+
 });
+
+
+// Dom loaded
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    breakpoint.refreshValue();
+
+    //console.log('Load Breakpoint ' + breakpoint.value);
+
+    // JS
+    var submenu = document.getElementsByClassName("AxaXl-Header__MainNav-link");
+
+    for (var i = 0; i < submenu.length; i++) {
+        submenu[i].addEventListener('click', menus, false);
+    }
+
+    function menus() {
+        this.parentElement.classList.toggle("AxaXl-Header__MainNav-list--active");
+    };
+
+});
+
