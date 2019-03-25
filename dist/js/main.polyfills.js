26098949b914c73294bf5a03b531de4bd1541888
Oody <oudinyt@gmail;com>
2019-03-25 15:12:01 +0100

    header coorection

--- dist/js/main.polyfills.js
+++ dist/js/main.polyfills.js
@@ -6,10 +6,45 @@
  * http://link-to-your-git-repo.com
  */
 
-document.addEventListener('click', (function (event) {
-	if (!event.target.matches('#click-me')) return;
-	alert('You clicked me!');
-}), false);
+// Breakpoint
+var breakpoint = {};
+
+breakpoint.refreshValue = function () {
+    this.value = window.getComputedStyle(document.querySelector('body'), ':before').getPropertyValue('content').replace(/\"/g, '');
+};
+
+
+// Resize
+window.addEventListener("resize", (function(event) {
+
+    breakpoint.refreshValue();
+
+    //console.log('resize Breakpoint ' + breakpoint.value);
+
+}));
+
+
+// Dom loaded
+document.addEventListener("DOMContentLoaded", (function(event) {
+
+    breakpoint.refreshValue();
+
+    //console.log('Load Breakpoint ' + breakpoint.value);
+
+    // JS
+    var submenu = document.getElementsByClassName("AxaXl-Header__MainNav-link");
+
+    for (var i = 0; i < submenu.length; i++) {
+        submenu[i].addEventListener('click', menus, false);
+    }
+
+    function menus() {
+        this.parentElement.classList.toggle("AxaXl-Header__MainNav-list--active");
+    };
+
+}));
+
+
 /**
  * Element.matches() polyfill (simple version)
  * https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
